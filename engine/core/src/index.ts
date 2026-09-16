ff6368360aed191e87c2b7c97685ff82b2a428a1
HarrowHaus <donald@harrow.haus>
2026-09-16 10:55:56 -0500

    Export seeded Event Director

--- engine/core/src/index.ts
+++ engine/core/src/index.ts
@@ -7,6 +7,7 @@ export * from "./combat/persistent.js";
 export * from "./procgen/floor.js";
 export * from "./procgen/run.js";
 export * from "./procgen/encounter.js";
+export * from "./procgen/event.js";
 export * from "./content/thresholds.js";
 export * from "./effects/effects.js";
 export * from "./content/floor1-effects.js";
