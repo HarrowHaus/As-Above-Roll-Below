6bd88b2d4dc3c14b4b1919a8cd5dbe495889aa1a
HarrowHaus <donald@harrow.haus>
2026-09-16 12:34:27 -0500

    Encode Floor I Boss Draft pool

---
+++ engine/core/src/content/floor1-rewards.ts
@@ -0,0 +1,10 @@
+export const floor1BossDraftItemIds = [
+  "breaching-bar",
+  "proof-vest",
+  "false-bottom",
+  "hidden-hand",
+  "opposite-number",
+  "carbon-paper",
+  "stuck-key",
+  "blank-face",
+] as const;
